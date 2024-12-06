6eb3e0152a8383cdac8fa7338cd1760831c6c1d1
Daniel <andersson.daniel1986@gmail.com>
2024-12-06 12:10:54 +0100

    test

---
+++ src/Login.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+class LoginTest:
+    def __init__(self, page):
+        self.page = page
+
+    def login(self, username: str, password: str):
+        """Perform login with provided credentials."""
+        self.page.goto("https://www.saucedemo.com/")
+        expect(self.page.locator("#user-name")).to_be_visible()
+        self.page.locator("#user-name").fill(username)
+        self.page.locator("[data-test=\"password\"]").fill(password)
+        self.page.locator("[data-test=\"login-button\"]").click()
+        expect(self.page.locator("#logout_sidebar_link")).to_be_enabled()
+
+
